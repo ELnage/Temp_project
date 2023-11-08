875fe4cc207732cdff9c1310aa9933bc9e489fe0
Andrew Magdy <89481184+Andrew-Magdy-Rizk@users.noreply.github.com>
2023-11-08 06:19:51 +0200

    links

--- JS/main.js
+++ JS/main.js
@@ -1,59 +1,67 @@
-   let slide = Array.from(document.querySelectorAll("#home .slide"));
-   let slidesCount = slide.length;
-   let currentSlide = 3;
-   let left = document.getElementById("left");
-   let right = document.getElementById("right");
-   let bullets = Array.from(document.querySelectorAll("#home .bullets li"));
+let slide = Array.from(document.querySelectorAll("#home .slide"));
+let slidesCount = slide.length;
+let currentSlide = 3;
+let left = document.getElementById("left");
+let right = document.getElementById("right");
+let bullets = Array.from(document.querySelectorAll("#home .bullets li"));
 
-   right.onclick = pervSlide;
-   left.onclick = nextSlide;
-   function nextSlide() {
-   removeActive();
-   if (currentSlide < slidesCount) {
-      currentSlide++;
-   } else {
-      currentSlide = 1;
-   }
-   cheker();
-   }
-   function pervSlide() {
-   removeActive();
-   if (currentSlide > 1) {
-      currentSlide--;
-   } else {
-      currentSlide = slidesCount;
-   }
-   cheker();
-   }
+right.onclick = pervSlide;
+left.onclick = nextSlide;
+function nextSlide() {
+  removeActive();
+  if (currentSlide < slidesCount) {
+    currentSlide++;
+  } else {
+    currentSlide = 1;
+  }
+  cheker();
+}
+function pervSlide() {
+  removeActive();
+  if (currentSlide > 1) {
+    currentSlide--;
+  } else {
+    currentSlide = slidesCount;
+  }
+  cheker();
+}
 
-   function removeActive() {
-   slide.forEach((e) => {
-      e.classList.remove("active");
-   });
+function removeActive() {
+  slide.forEach((e) => {
+    e.classList.remove("active");
+  });
 
-   bullets.forEach((e) => {
-      e.classList.remove("active");
-   });
-   }
-   function cheker() {
-   slide[currentSlide - 1].classList.add("active");
-   bullets[currentSlide - 1].classList.add("active");
-   }
-   bullets.forEach((e) => {
-   e.onclick = function () {
-      currentSlide = e.getAttribute("data-index");
-      removeActive();
-      cheker();
-   };
-   });
+  bullets.forEach((e) => {
+    e.classList.remove("active");
+  });
+}
+function cheker() {
+  slide[currentSlide - 1].classList.add("active");
+  bullets[currentSlide - 1].classList.add("active");
+}
+bullets.forEach((e) => {
+  e.onclick = function () {
+    currentSlide = e.getAttribute("data-index");
+    removeActive();
+    cheker();
+  };
+});
 
-   setInterval(() => {
-   if (currentSlide > 1) {
-      currentSlide--;
-   } else {
-      currentSlide = slidesCount;
-   }
-   removeActive();
-   cheker();
-   }, 6000);
-   cheker();
+setInterval(() => {
+  if (currentSlide > 1) {
+    currentSlide--;
+  } else {
+    currentSlide = slidesCount;
+  }
+  removeActive();
+  cheker();
+}, 6000);
+cheker();
+
+let links = document.getElementById("links");
+let icon = document.getElementById("icon");
+window.onload = () => {
+  icon.addEventListener("click", () => {
+    links.style.cssText = "display: block; left: 0;";
+  });
+};
